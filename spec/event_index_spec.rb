4604aff5777cc10dc91d200a05b7518193bb32ee
Josh Ferguson <Josh@Besquared.local>
2010-04-16 00:00:07 -0700

    adding sampling, not working quite right just yet

--- spec/event_index_spec.rb
+++ spec/event_index_spec.rb
@@ -2,12 +2,21 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Chronologic::EventIndex do
   before(:each) do
-    
+    @event = {:id => 1, :created_at => Time.now.utc.to_i}
   end
   
   it "should insert an event" do
-    event = {:created_at => Time.now.utc.to_i}
     index = Chronologic::EventIndex.new('registrations')
-    index.insert(event)
+    index.insert(@event).should == true
+  end
+  
+  it "should sample all events" do
+    index = Chronologic::EventIndex::new('registrations')
+    
+    100.times do |num|
+      index.insert(@event.merge(:id => num))
+    end
+    
+    puts index.sample("").inspect
   end
 end
--- src/chronologic/event_index.rb
+++ src/chronologic/event_index.rb
@@ -9,31 +9,53 @@ module Chronologic
     end
     
     def insert(event = {})
+      return false unless event.has_key?(:id)
+      
+      event.merge!(:random_key => rand)
+      
       index = lucene.index.IndexWriter.new(directory, analyzer, true, max_field_length)
       
-      # id_text_pair_array.each {|id_text_pair|
-      #   term_to_delete = lucene.index.Term.new("id", id_text_pair[0].to_s) # if it exists
-      #   a_document = lucene.document.Document.new
-      #   a_document.add(lucene.document.Field.new('text', id_text_pair[1],
-      #                    lucene.document.Field::Store::YES,
-      #                    lucene.document.Field::Index::TOKENIZED))
-      #   a_document.add(lucene.document.Field.new('id', id_text_pair[0].to_s,
-      #                    lucene.document.Field::Store::YES,
-      #                    lucene.document.Field::Index::TOKENIZED))
-      #   index_writer.updateDocument(term_to_delete, a_document) # delete any old docs with same id
-      # }
-      
+      document = lucene.document.Document.new
+      event.each do |fname, value|
+        if fname == :id
+          field = lucene.document.Field.new(fname.to_s, value.to_s, stored, not_analyzed)
+        else
+          field = lucene.document.Field.new(fname.to_s, value.to_s, not_stored, not_analyzed)
+        end
+        document.add(field)
+      end
+      index.addDocument(document)
       index.close
+      true
     end
     
-    def update(key, event = {})
-    end
-    
-    def remove(key)
+    def sample(query_string, options = {:count => 10})
+      searcher = lucene.search.IndexSearcher.new(directory);
+      
+      if query_string.empty?
+        query = lucene.search.MatchAllDocsQuery.new
+      else
+        parser = lucene.queryParser.QueryParser.new(lucene_version, "random_key", analyzer);
+        query = parser.parse(query_string);
+      end
+      
+      sort = lucene.search.Sort.new(random_sort_field)
+      collector = lucene.search.TopFieldCollector.create(sort, options[:count], false, false, false, false);
+      searcher.search(query, collector);
+      
+      hits = collector.topDocs.scoreDocs
+      
+      results = []
+      0.upto(hits.length - 1) do |doc_num|
+        results << searcher.doc(hits[doc_num].doc).get("id")
+      end
+      
+      searcher.close
+      
+      results
     end
     
   protected
-    
     def path
       File.join(Configuration.instance.index_path, "events", name)
     end
@@ -43,7 +65,7 @@ module Chronologic
     end
     
     def analyzer
-      lucene.analysis.standard.StandardAnalyzer.new(lucene.util.Version::LUCENE_30)
+      lucene.analysis.standard.StandardAnalyzer.new(lucene_version)
     end
     
     def max_field_length
@@ -52,6 +74,26 @@ module Chronologic
     
     def exists
       lucene.index.IndexReader.index_exists(directory)
+    end
+    
+    def stored
+      lucene.document.Field::Store::YES
+    end
+    
+    def not_stored
+      lucene.document.Field::Store::NO
+    end
+    
+    def not_analyzed
+      lucene.document.Field::Index::NOT_ANALYZED
+    end
+    
+    def lucene_version
+      lucene.util.Version::LUCENE_30
+    end
+    
+    def random_sort_field
+      lucene.search.SortField.new("random_key", lucene.search.SortField::DOUBLE)
     end
   end
 end
